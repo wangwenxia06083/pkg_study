8729a62ca9e75fdbca0060edc2c3b8bef6d3f7da
wwwang06083 <wwwang06083@163.com>
2017-11-19 14:19:06 +0800

    SPARK UPDATE

--- sparkR.R
+++ sparkR.R
@@ -1,1 +1,4 @@
-gjgjgj
+sc <- spark_connect(master = "local")
+testing <- spark_read_csv(sc,'testing',path = 'E:/R/test_data/revenue_new.csv')
+library(DBI)
+dbGetQuery(sc,"select * from testing limit 5")
